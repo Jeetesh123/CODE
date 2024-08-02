d76769480ee28d4e564823b96fc9a38018cb9846
Jeetesh123 <53112585+Jeetesh123@users.noreply.github.com>
2024-08-02 17:50:17 +0530

    added LinkedList folder and code

---
+++ LinkedList/Find Middle Node Of LinkedList/Solution.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+
+        //Time Complexity - O(n); // need to traverse almost all elements
+        //Spae Complexity - O(1); // not using any extra data structure for storage
+        
+    }
+}
